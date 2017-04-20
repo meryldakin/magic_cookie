ae7b9e25c29a229864a81d241388201425c54680
meryldakin <meryl.k.dakin@gmail.com>
2017-04-20 16:49:20 -0400

    fixed some things

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,15 +23,15 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = current_user
+    @user = User.find(params[:id])
   end
 
   def edit
-    @user =  current_user
+    @user = User.find(params[:id])
   end
 
   def update
-    @user = current_user
+    @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user)
   end
